d7544a8e39a83c1d7bfe8183adc4ce9e42ed041e
Weizhi Yang <wyang@adobe.com>
2017-12-15 13:52:38 -0800

    update to ubunto 16, and install Anaconda

--- Vagrantfile
+++ Vagrantfile
@@ -1,16 +1,17 @@
 # -*- mode: ruby -*-
 # vi: set ft=ruby :
 
-# vbox_box = "precise64"
-# vbox_url = "http://files.vagrantup.com/precise64.box"
+vbox_box = "ubuntu/xenial64"
 
 black_sails_datasource = "../black-sails-datasource/"
+black_sails_ctp = "../black-sails-ctp/"
 
 Vagrant.configure("2") do |config|
   config.vm.define "datasource" do |datasource|
-	datasource.vm.box = "hashicorp/precise64"
-	datasource.vm.synced_folder black_sails_datasource, "/home/vagrant/black_sails_datasource"
-	datasource.vm.synced_folder "thirdparty", "/home/vagrant/thirdparty"
+	datasource.vm.box = vbox_box
+	datasource.vm.synced_folder black_sails_datasource, "/home/ubuntu/black_sails_datasource"
+	datasource.vm.synced_folder black_sails_ctp, "/home/ubuntu/black_sails_ctp"
+	datasource.vm.synced_folder "thirdparty", "/home/ubuntu/thirdparty"
 	datasource.vm.provision "shell", path: "config.sh"
   end
 end
--- config.sh
+++ config.sh
@@ -1,18 +1,29 @@
 sudo apt-get update
-sudo apt-get install python
+
 # install python3.4, and pip3.4
-sudo apt-get install python-software-properties -y
-echo | sudo add-apt-repository ppa:fkrull/deadsnakes
-sudo apt-get update
-sudo apt-get install python3.4 -y
-sudo python3.4 /home/vagrant/thirdparty/get-pip.py
+# sudo apt-get install python-software-properties -y
+# echo | sudo add-apt-repository ppa:fkrull/deadsnakes
+# sudo apt-get update
+# # sudo apt-get install python3.4 -y
+# sudo python3.4 /home/ubuntu/thirdparty/get-pip.py
 
 #install and setup emcas25
 echo | sudo add-apt-repository ppa:kelleyk/emacs
 sudo apt-get update
 sudo apt-get install emacs25 -y
-mkdir /home/vagrant/.emacs.d
-cp thirdparty/init.el /home/vagrant/.emacs.d/init.el
-cp -r thirdparty/themes /home/vagrant/.emacs.d/
+sudo -u ubuntu mkdir -p /home/ubuntu/.emacs.d
+cp thirdparty/init.el /home/ubuntu/.emacs.d/init.el
+cp -r thirdparty/themes /home/ubuntu/.emacs.d/
+
+#install Anaconda
+sudo chmod +x /home/ubuntu/thirdparty/Anaconda3-5.0.1-Linux-x86_64.sh
+sudo -u ubuntu /home/ubuntu/thirdparty/Anaconda3-5.0.1-Linux-x86_64.sh -b
+echo 'export PATH=/home/ubuntu/anaconda3/bin:$PATH' >>/home/ubuntu/.profile
+source /home/ubuntu/.profile
+conda create --name python27 python=2.7 -y
+source activate python27
+
+#install libs for building vnpy
+apt-get install -y build-essential libboost-all-dev python-dev cmake
 
 
